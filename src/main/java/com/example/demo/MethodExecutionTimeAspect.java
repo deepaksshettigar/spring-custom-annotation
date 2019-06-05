7e5d64b093bcbfb06fd60c8bca98e58649d9a99c
Deepak Shettigar <deepaksshettigar@gmail.com>
2019-06-06 03:02:53 +0530

    initial load

---
+++ src/main/java/com/example/demo/MethodExecutionTimeAspect.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import java.lang.reflect.Method;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MethodExecutionTimeAspect {
+
+    @Around("@annotation(LogExecutionTime)")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        final long start = System.currentTimeMillis();
+        
+        final Object proceed = joinPoint.proceed();
+        
+
+        final long executionTime = System.currentTimeMillis() - start;
+
+        System.out.println("Target : " + joinPoint.getTarget()  + ". " + joinPoint.getSignature() + " executed in " + executionTime + "ms. Kind :" + joinPoint.getKind() );
+        
+		/*
+		 * Method mth = null; try { mth = (Method)
+		 * joinPoint.getStaticPart().getSignature(); } catch (SecurityException e1) { //
+		 * TODO Auto-generated catch block e1.printStackTrace(); } LogExecutionTime
+		 * annotation = mth.getAnnotation(LogExecutionTime.class);
+		 * System.out.println("annotation enabled ::: " + annotation.enabled());
+		 */
+        
+        return proceed;
+    }
+
+}
